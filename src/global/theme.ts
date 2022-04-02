9ca70b1d2b0a52ab56fbcebbfdcd43a5fe222e06
José Octávio <joseoctavioconti@gmail.com>
2022-04-02 20:53:28 -0300

    Change theme to native base

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,9 +1,45 @@
-export const theme = {
+import { extendTheme } from 'native-base';
+
+export const theme = extendTheme({
+  fontConfig: {
+    Roboto: {
+      400: {
+        normal: 'Roboto_400Regular',
+      },
+      500: {
+        normal: 'Roboto_500Medium',
+      },
+      700: {
+        normal: 'Roboto_700Bold',
+      },
+    },
+  },
+
+  fonts: {
+    heading: 'Roboto_700Bold',
+    body: 'Roboto_400Regular',
+    mono: 'Roboto_500Medium',
+  },
+
   components: {
+    Heading: {
+      defaultProps: {
+        color: 'gray.300',
+      },
+    },
     Text: {
-      fontSize: 16,
-      fontFamily: 'Roboto_500Medium',
-      color: 'gray300'
-    }
-  }
-}
+      defaultProps: {
+        fontFamily: 'body',
+        color: 'gray.300',
+      },
+    },
+    Button: {
+      defaultProps: {
+        _text: {
+          fontFamily: 'heading',
+          fontSize: 16,
+        }
+      },
+    },
+  },
+});
